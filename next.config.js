098d6641209a31fa1df46f6c8b4bd728d186446d
Johnny <41584622+jonipwi@users.noreply.github.com>
2025-11-08 04:10:58 +0700

    fix: bugs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE || '/petition',
   },
